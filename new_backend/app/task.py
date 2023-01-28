6d645a1409c8ccf3dcdb30f9fd4180ba3a5b8dfd
TopEngineer0926 <top_engineer_0926@outlook.com>
2023-01-28 15:10:48 -0500

    removed empty item in prompt response data

--- new_backend/app/task.py
+++ new_backend/app/task.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import asyncio
 import openai_async
+import time
 import os
 
 
@@ -15,16 +16,21 @@ Here is an ordered list of the companies from the text:
     temperature = 0
     temp_data = (prompt_prepend + prompt).strip()
     temp_list = temp_data.split('\n')
+    data_list = [x for x in temp_list if x != '']
     n= 120 # word count
     # split prompt data
-    split_data = [(temp_list[i:i+n]) for i in range(0, len(temp_list), n)]
+    split_data = [(data_list[i:i+n]) for i in range(0, len(data_list), n)]
     total_response_list = []
     prompt_response = {}
 
-    async def task_coro(item):
+    async def delay_time():
+        time.sleep(1.65)
+
+    async def task_coro(item, index):
         # report a message
         print("".join(item) + prompt)
         temp_prompt = "".join(item) + prompt
+        await delay_time()
         response = await openai_async.complete(
             key,
             timeout=50,
@@ -38,20 +44,21 @@ Here is an ordered list of the companies from the text:
                 'stop':"####"
             },
         )
+        print(response.json()["choices"][0]["text"].translate({ord('\t'):None}).split('\n'))
         if("error" in response.json()):
             print(response.json()["error"])
         else:
             response_list =  response.json()["choices"][0]["text"].translate({ord('\t'):None}).split('\n')
             prompt_response[temp_prompt] = response_list
-            total_response_list.append(response_list[0])
+            total_response_list.append(response_list[0].stript(''))
             print(response_list)
             for i, item in enumerate(response_list):
-                print(item)
+                # print(item)
                 if i == 0:
                     continue
                 if(len(item.split(". ")) > 1):
                     total_response_list.append(item.split(". ")[1])
-        print(type(total_response_list))
+        # print(type(total_response_list))
         print ('task end')
  
     # coroutine used for the entry point
@@ -59,7 +66,7 @@ Here is an ordered list of the companies from the text:
         # report a message
         print('main starting')
         # create many coroutines
-        coros = [task_coro(item) for i, item in enumerate(split_data)]
+        coros = [task_coro(item, i) for i, item in enumerate(split_data)]
         # run the tasks
         await asyncio.gather(*coros)
         print(total_response_list)
