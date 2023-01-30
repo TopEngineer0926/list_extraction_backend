56615445e3f52b898dd6ee68850136e700dbf1dc
TopEngineer0926 <top_engineer_0926@outlook.com>
2023-01-30 04:22:26 -0500

    removed coroutines and cleanup response list

--- new_backend/app/task.py
+++ new_backend/app/task.py
@@ -22,35 +22,30 @@ Here is an ordered list of the companies from the text:
     split_data = [(data_list[i:i+n]) for i in range(0, len(data_list), n)]
     total_response_list = []
     prompt_response = {}
-
-    async def main():
-        # report a message
-        print('main starting')
-        # run the tasks
-        prompt_prepend = ""
-        for i, item in enumerate(split_data):
-            temp_prompt = "".join(item) + prompt
-            response = oa.Completion.create(
-                model="code-davinci-002",
-                prompt=prompt_prepend + temp_prompt,
-                temperature=temperature,
-                max_tokens=400,
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0,
-                stop="####"
-            )
-            response_list = response["choices"][0]["text"].translate({ord('\t'):None}).split('\n')
-            response_list = [x for x in response_list if x != '']
-            prompt_prepend += str(response_list)
-            print(response_list)
-            prompt_response[temp_prompt] = response_list
-        total_response_list.append(response_list[0].strip(''))
-        for i, item in enumerate(response_list):
-            if i == 0:
-                continue
-            if(len(item.split(". ")) > 1):
-                total_response_list.append(item.split(". ")[1])
-        print('main done')
-    asyncio.run(main())
-    return {"return_data": total_response_list, "prompt_response": prompt_response}
+    prompt_prepend = ""
+    for i, item in enumerate(split_data):
+        temp_prompt = "".join(item) + prompt
+        response = oa.Completion.create(
+            model="code-davinci-002",
+            prompt=prompt_prepend + temp_prompt,
+            temperature=temperature,
+            max_tokens=400,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop="####"
+        )
+        response_list = response["choices"][0]["text"].translate({ord('\t'):None}).split('\n')
+        response_list = [x for x in response_list if x != '']
+        prompt_prepend += str(response_list)
+        print(response_list)
+        prompt_response[temp_prompt] = response_list
+    total_response_list.append(response_list[0].strip(''))
+    for i, item in enumerate(response_list):
+        if i == 0:
+            continue
+        if(len(item.split(". ")) > 1):
+            total_response_list.append(item.split(". ")[1])
+    cleanup_response_list = []
+    [cleanup_response_list.append(item) for item in total_response_list if item not in cleanup_response_list]
+    return {"return_data": cleanup_response_list, "prompt_response": prompt_response}
