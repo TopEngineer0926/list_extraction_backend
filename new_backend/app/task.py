649f3a5db81bd0ef3a0d65ad2a4b78f4bff1668e
TopEngineer0926 <top_engineer_0926@outlook.com>
2023-01-30 00:32:25 -0500

    Prepended prompts with results of previous prompts.

--- new_backend/app/task.py
+++ new_backend/app/task.py
@@ -1,20 +1,20 @@
 from dotenv import load_dotenv
 import asyncio
-import openai_async
-import time
+import openai as oa
 import os
 
 
 def openai(list_text: str):
     load_dotenv()
+    oa.api_key= os.getenv("OPENAI_API_KEY")
     key = os.getenv("OPENAI_API_KEY")
-    prompt_prepend = list_text
     prompt = """
 ####
 Here is an ordered list of the companies from the text:
 1."""
+
     temperature = 0
-    temp_data = (prompt_prepend + prompt).strip()
+    temp_data = (list_text + prompt).strip()
     temp_list = temp_data.split('\n')
     data_list = [x for x in temp_list if x != '']
     n= 120 # word count
@@ -23,53 +23,34 @@ Here is an ordered list of the companies from the text:
     total_response_list = []
     prompt_response = {}
 
-    async def delay_time():
-        time.sleep(1.65)
-
-    async def task_coro(item, index):
-        # report a message
-        print("".join(item) + prompt)
-        temp_prompt = "".join(item) + prompt
-        await delay_time()
-        response = await openai_async.complete(
-            key,
-            timeout=50,
-            payload={
-                "model": "code-davinci-002",
-                "prompt": temp_prompt,
-                "temperature": temperature,
-                "top_p":1,
-                "frequency_penalty":0,
-                "presence_penalty":0,
-                'stop':"####"
-            },
-        )
-        print(response.json()["choices"][0]["text"].translate({ord('\t'):None}).split('\n'))
-        if("error" in response.json()):
-            print(response.json()["error"])
-        else:
-            response_list =  response.json()["choices"][0]["text"].translate({ord('\t'):None}).split('\n')
-            prompt_response[temp_prompt] = response_list
-            total_response_list.append(response_list[0].stript(''))
-            print(response_list)
-            for i, item in enumerate(response_list):
-                # print(item)
-                if i == 0:
-                    continue
-                if(len(item.split(". ")) > 1):
-                    total_response_list.append(item.split(". ")[1])
-        # print(type(total_response_list))
-        print ('task end')
- 
-    # coroutine used for the entry point
     async def main():
         # report a message
         print('main starting')
-        # create many coroutines
-        coros = [task_coro(item, i) for i, item in enumerate(split_data)]
         # run the tasks
-        await asyncio.gather(*coros)
-        print(total_response_list)
-        print('main done') 
+        prompt_prepend = ""
+        for i, item in enumerate(split_data):
+            temp_prompt = "".join(item) + prompt
+            response = oa.Completion.create(
+                model="code-davinci-002",
+                prompt=prompt_prepend + temp_prompt,
+                temperature=temperature,
+                max_tokens=400,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                stop="####"
+            )
+            response_list = response["choices"][0]["text"].translate({ord('\t'):None}).split('\n')
+            response_list = [x for x in response_list if x != '']
+            prompt_prepend += str(response_list)
+            print(response_list)
+            prompt_response[temp_prompt] = response_list
+        total_response_list.append(response_list[0].strip(''))
+        for i, item in enumerate(response_list):
+            if i == 0:
+                continue
+            if(len(item.split(". ")) > 1):
+                total_response_list.append(item.split(". ")[1])
+        print('main done')
     asyncio.run(main())
     return {"return_data": total_response_list, "prompt_response": prompt_response}
