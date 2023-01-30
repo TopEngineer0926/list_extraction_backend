907153a036bc3650c54687c9e160a4aa95dcba0e
TopEngineer0926 <top_engineer_0926@outlook.com>
2023-01-30 14:23:51 -0500

    modified prompt

--- new_backend/app/task.py
+++ new_backend/app/task.py
@@ -5,19 +5,23 @@ import os
 
 
 def openai(list_text: str):
+    print("#openai start")
     load_dotenv()
     oa.api_key= os.getenv("OPENAI_API_KEY")
     key = os.getenv("OPENAI_API_KEY")
     prompt = """
 ####
 Here is an ordered list of the companies from the text:
-1."""
+"""
 
     temperature = 0
     temp_data = (list_text + prompt).strip()
     temp_list = temp_data.split('\n')
     data_list = [x for x in temp_list if x != '']
-    n= 120 # word count
+    for item in data_list:
+        print("**************************")
+        print(item)
+    n= 120 # sentence count
     # split prompt data
     split_data = [(data_list[i:i+n]) for i in range(0, len(data_list), n)]
     total_response_list = []
@@ -40,12 +44,11 @@ Here is an ordered list of the companies from the text:
         prompt_prepend += str(response_list)
         print(response_list)
         prompt_response[temp_prompt] = response_list
-    total_response_list.append(response_list[0].strip(''))
     for i, item in enumerate(response_list):
-        if i == 0:
-            continue
         if(len(item.split(". ")) > 1):
             total_response_list.append(item.split(". ")[1])
     cleanup_response_list = []
     [cleanup_response_list.append(item) for item in total_response_list if item not in cleanup_response_list]
+    print(cleanup_response_list)
+    print("#openai end")
     return {"return_data": cleanup_response_list, "prompt_response": prompt_response}
