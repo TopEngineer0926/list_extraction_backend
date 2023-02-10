8a51341b751841d95ca6ae2520e44ae408675fef
TopEngineer0926 <top_engineer_0926@outlook.com>
2023-02-10 02:12:06 -0500

    revert

--- main.py
+++ main.py
@@ -1,136 +1,66 @@
-from fastapi import FastAPI, HTTPException, Depends, Body
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from database import SessionLocal, engine
 from dotenv import load_dotenv
-import openai_async
-import asyncio
+from typing import Union, Optional
 import logging
 import openai
 import os
-from schema import DataList
-import crud, models
-
-class Item(BaseModel):
-    title: str
-    list_text: str
-
-models.Base.metadata.create_all(bind=engine)
 
 load_dotenv()
 app = FastAPI()
 logger = logging.getLogger()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-key = os.getenv("OPENAI_API_KEY")
 
-def db():
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
+@app.get("/")
+async def root(request: Request):
+    return "works"
 
-@app.post('/api/list_text')
-def api(item: Item = Body(), db=Depends(db)):
 
-    title = item.title
-    list_text = item.list_text
+@app.post("/api")
+async def api(list_text: str):
+
     prompt_prepend = list_text
+
+
+    #with open('test_text.txt','r') as file:
+        #prompt_prepend = file.read()
+
+    #logger.error("prompt_prepend is " + prompt_prepend)
+
     prompt = """
 ####
 
 Here is an ordered list of the companies from the text:
 
 1."""
-
     temperature = 0
 
-    temp_data = prompt_prepend.strip()
+    temp_data = (prompt_prepend + prompt).strip()
     temp_list = temp_data.split('\n')
-    n= 150 # word count
+    n= 1200 # word count
     # split prompt data
     split_data = [(temp_list[i:i+n]) for i in range(0, len(temp_list), n)]
+    print(split_data)
 
-    total_response_list = []
-    prompt_response = {}
-    record_id = []
-    
-    async def task_coro(item):
-        # report a message
-        print("".join(item) + prompt)
-        temp_prompt = "".join(item) + prompt
-        response = await openai_async.complete(
-            key,
-            timeout=50,
-            payload={
-                "model": "code-davinci-002",
-                "prompt": temp_prompt,
-                "temperature": temperature,
-                "top_p":1,
-                "frequency_penalty":0,
-                "presence_penalty":0,
-                'stop':"####"
-            },
-        )
-        if("error" in response.json()):
-            print(response.json()["error"])
-        else:
-            response_list =  response.json()["choices"][0]["text"].translate({ord('\t'):None}).split('\n')
-            prompt_response[temp_prompt] = response_list
-            total_response_list.append(response_list[0])
-            print(response_list)
-            for i, item in enumerate(response_list):
-                print(item)
-                if i == 0:
-                    continue
-                if(len(item.split(". ")) > 1):
-                    total_response_list.append(item.split(". ")[1])
-        print ('task end')
- 
-    # coroutine used for the entry point
-    async def main():
-        # report a message
-        print('main starting')
-        # create many coroutines
-        coros = [task_coro(item) for i, item in enumerate(split_data)]
-        # run the tasks
-        await asyncio.gather(*coros)
-        info = DataList(
-                title= title,
-                request= list_text,
-                prompt_response= str(prompt_response),
-                return_data= str(total_response_list)
-        )
-        record = await crud.save_datalist_info(db,info)
-        # report a message
-        record_id.append(record.id)
-        print('main done') 
-    asyncio.run(main())
-    return {"list": total_response_list, "id": record_id[0]}
-
-@app.post('/api/list_extraction')
-def save_datalist_info(item = Body(), db=Depends(db)):
-    id = item['id']
-    title = item['title']
-    return_data = item['list']
-    data = DataList(
-        id= id,
-        title = title,
-        return_data= str(return_data)
+    response = openai.Completion.create(
+        model="code-davinci-002",
+        prompt=prompt_prepend + prompt,
+        temperature=temperature,
+        max_tokens=400,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop="####"
     )
-    post_query = db.query(models.DataList).filter(models.DataList.id == id)
-    updated_post = post_query.first()
-    post_query.update(data.dict(exclude_unset=True), synchronize_session=False)
-    db.commit()
 
-@app.get('/datalist/info/{id}')
-def get_datalist_info(token: str, db=Depends(db)):
-    info = crud.get_datalist_info(db,id)
-    if info:
-        return info
-    else:
-        raise HTTPException(404, crud.error_message('No device found for token {}'.format(token)))
+    response_list = response['choices'][0]['text'].strip().split("\n")
+    parsed_list = [response_list[0]]
 
-@app.get('/device/info')
-def get_all_datalist_info(db=Depends(db)):
-    return crud.get_datalist_info(db)
+    for i, item in enumerate(response_list):
+        if i == 0:
+            continue
+        if(len(item.split(". ")) > 1):
+            parsed_list.append(item.split(". ")[1])
+
+    return {"list": parsed_list}
--- new_backend/app/crud/__init__.py
+++
@@ -1,1 +0,0 @@
-from .crud_datalist import datalist
